8ada6ce6ef57fb873448d8a09a1e31a1ea7a491b
Joseph Kabau <Joseph.Kabau@britehouse.co.za>
2019-11-07 10:32:00 +0200

    added index[1] on new_random_product object

--- arc.py
+++ arc.py
@@ -121,7 +121,7 @@ def create_dummmy_item():
         'product_name': random_item,
         'product_price': item_price,
         'manufacturer': 'Evil Corp'
-    })
+    }, index=[1])
     new_random_product.to_sql('Checkout', engine, if_exists='append', index=False)
 
 
